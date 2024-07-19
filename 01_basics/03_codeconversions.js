11268811217bb1620c2e792f53654e775ff708e9
Anakin828 <engg74698@gmail.com>
2024-07-19 23:50:47 +0530

    codeconversion

---
+++ 01_basics/03_codeconversions.js
@@ -0,0 +1,17 @@
+let score=null
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(type valueInNumber);
+console.log(valueInNumber);
+
+//"33"=> 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIN =1
+
+let booleanIsLOggedIn = boolean(isLoggedIn)
+console.log();
