523a52b5cb38301e07c709dd6891bc3125c613c9
Anakin828 <engg74698@gmail.com>
2024-07-19 07:28:09 +0000

    variables learn

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accountEmail="badal@123"
+var accountPassword="12345"
+accountCity="Faridabad"
+
+// accountId=2
+
+accountEmail="hc@hc.com"
+accountPassword="212121"
+accountCity="Bengaluru"
+let accountState;
+
+console.log();
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState])
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope */
