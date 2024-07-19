d9831400a531b3efbecf635b31fac87cdf2da1d7
Anakin828 <engg74698@gmail.com>
2024-07-19 09:29:37 +0000

    variable

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";// treat all JS code as newer version
+
+//alert(3+3); //we are using node js not browser
+
+console.log(3+3)//code reusability should br high
+console.log("Badal")
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+let state;
+// number=>2 to power 53
+// bigInt
+// string=>""
+//bolean=> true/false
+// null=> standalone value and also datatype
+//undefined =>
+    // symbol=> unique
+
+//object
+
+console.log(typeof"hitesh");
