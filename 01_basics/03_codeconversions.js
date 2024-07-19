d9ad49af7600344b96c5a304164926f15f18264f
Anakin828 <engg74698@gmail.com>
2024-07-19 19:12:17 +0000

    conversion operation

--- 01_basics/03_codeconversions.js
+++ 01_basics/03_codeconversions.js
@@ -4,14 +4,24 @@ console.log(typeof score)
 console.log(typeof(score));
 
 let valueInNumber=Number(score)
-console.log(type valueInNumber);
+console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
 //"33"=> 33
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIN =1
+let isLoggedIn =1
 
-let booleanIsLOggedIn = boolean(isLoggedIn)
-console.log();
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 =>true; 0 => false
+// ""=>false
+// "hitesh"=> true
+
+let someNumber =33
+
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
