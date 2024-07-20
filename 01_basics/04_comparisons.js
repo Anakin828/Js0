cbac60526e4dec49d008173592a1757f9762ca45
Anakin828 <engg74698@gmail.com>
2024-07-20 06:12:42 +0000

    comparison

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,23 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+//=== ane wo check karta he ki same datatype he ki nhi to ye dono sme datatype he hi nhi thats whi the output will be false
+
+console.log("2"===2);
+
+// Is tarah ke comparisons ko hum avoid karte he because clean code is our priority
