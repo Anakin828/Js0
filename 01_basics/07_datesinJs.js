a2ce31fe33fbf9b651af5a6639bb3d922cf1722f
Anakin828 <engg74698@gmail.com>
2024-07-21 10:41:30 +0000

    date and time in Js

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -7,6 +7,32 @@ let myDate=new Date()
 // date is an object-------Interview questions
 
 // let myCreatedDate = new Date(2024,0,23)
- let myCreatedDate = new Date(2024,0,23,5,3)
-console.log(myCreatedDate.toLocaleString());
+//  let myCreatedDate = new Date(2024,0,23,5,3)
+// console.log(myCreatedDate.toLocaleString());
 // In Javascript month starts from 0 that means january is 0
+let myCreatedDate= new Date("01-14-2023")
+console.log(myCreatedDate.toLocaleString());
+// yaha lekin january 01 se shuru hota he 00 se nhi
+
+let myTimeStamp = Date.now()
+// exact time stamp ka value chaiye to use ata he
+//  jese ki koi poll ya phir koi quiz
+ 
+
+//console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+// to redu ce the value of milliseconds and then floor to remove the decimal value
+
+let newDate=new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+// `${newDate.getday()} and the time `
+
+newDate.toLocaleString('default',{
+    weekday:"long",
+    // so we can acrually define timezone and weekday like mon or monday in long or short format in object
+})
+console.log(newDate)
