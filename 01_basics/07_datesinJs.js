259ffbfd01c0527ceabd94dbfd6c87d6d39967a0
Anakin828 <engg74698@gmail.com>
2024-07-21 06:57:53 +0000

    date and time

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,12 @@
+// Dates
+
+let myDate=new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate)
+// date is an object-------Interview questions
+
+// let myCreatedDate = new Date(2024,0,23)
+ let myCreatedDate = new Date(2024,0,23,5,3)
+console.log(myCreatedDate.toLocaleString());
+// In Javascript month starts from 0 that means january is 0
