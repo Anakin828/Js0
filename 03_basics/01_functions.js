a8c3080eb41580ac55282ce6790c869313c7bb7b
Anakin828 <engg74698@gmail.com>
2024-07-24 08:35:32 +0000

    functions using arrays

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,3 +30,36 @@ function loginUserMessage(username="sam"){
     return `${username} just logged in`
 }
 console.log(loginUserMessage())
+
+
+function calculateCartPrice(val1, val2,...num1){
+    // console.log(val1)
+    return  num1
+ 
+}
+console.log(calculateCartPrice(200,400,500,2000))
+
+const user={
+    username:"hitesh",
+    price:199
+}
+
+function handleobject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+handleobject(user)
+handleobject({
+    username:"sam",
+    price:199
+})
+
+const myNewArray= [200,400,600,1200]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,600,1200]));
+
