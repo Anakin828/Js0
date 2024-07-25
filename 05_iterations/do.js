eae1d00b3031e7094064f477bbc12d570e6052f1
Anakin828 <engg74698@gmail.com>
2024-07-25 16:26:23 +0000

    do loop in js

---
+++ 05_iterations/do.js
@@ -0,0 +1,22 @@
+
+let index=0 // initilaise bhi karna padega
+while (index<=10) {//(condition should be written in the parantheseis)
+    console.log(`value of index is:${index}`);
+    index=index+2
+    
+}
+
+let myarray=['flash','batman','superman']
+
+let arr=0
+while(arr<myarray.length){
+    console.log(`value is${myarray[arr]}`);
+    arr=arr+1;
+}
+
+let score=1
+do{
+    console.log(`score is ${score}`);
+    score++//pehle kam hota he phir condition check karte he
+
+}while(score<=10);
